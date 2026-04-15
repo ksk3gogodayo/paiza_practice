9dde9d154dadc4353ab1c9c851d3d0f2dacfb439
“keinet” <keisukeikegami15@gmail.com>
2026-04-15 12:54:09 +0900

    D185 時給の計算クリア！

--- paiza_test.js
+++ paiza_test.js
@@ -1,1 +1,16 @@
-﻿console.log('ケイさん');
+﻿// paizaで入力値を受け取るための決まり文句や！
+const fs = require('fs');
+
+function main() {
+    // 1.入力を全部読み込んで、改行や空白でばらばらにする
+    const input = fs.readFileSync('/dev/stdin', 'utf8').trim().split(/\s+/);
+    
+    // 2.一つ目が時給（N)、二つ目が時間（M）
+    // ※入力は「文字列」やから、Number（）で数字に変換
+    const N = Number(input [0]); 
+    const M = Number(input [1]); 
+    //　3.掛け算して出力
+        console.log(N * M);
+}
+
+main();
